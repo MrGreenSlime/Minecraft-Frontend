cc55e8c19c911360157eaf88bf24058a3f9ed5fe
lubu_ <luca.vandenweghe@student.odisee.be>
2024-05-21 18:30:33 +0200

    Initial commit Nuxt-App

---
+++ components/StarsAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
+
+<style scoped>
+@import '@/assets/scss/backgroundStars.scss';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <StarsAnimation />
+    <DeviceNotification />
+    <Header />
+    <SideNav />
+    <main>
+      <Intro />
+      <TeamSlider />
+    </main>
+  </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue'
+import StarsAnimation from '@/components/StarsAnimation'
+import DeviceNotification from '@/components/DeviceNotification.vue'
+import Header from '@/components/Header.vue'
+import SideNav from '@/components/SideNav.vue'
+import Intro from '@/components/Intro.vue'
+import TeamSlider from '@/components/TeamSlider.vue'
+
+export default defineComponent({
+  components: {
+    StarsAnimation,
+    DeviceNotification,
+    Header,
+    SideNav,
+    Intro,
+    TeamSlider
+  }
+})
+
+
+</script>
